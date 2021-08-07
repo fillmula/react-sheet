d56ed21763a4b6b42f4703083715340579d5d42a
Victor Teo <yeannylam@gmail.com>
2021-08-07 12:20:12 +0800

    fix bug

--- src/useSheetState.ts
+++ src/useSheetState.ts
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { pushStateRecord, removeStateRecord } from 'react-backable'
 import generateId from './generateId'
 
-const useSheetState = (initial: boolean) => {
+const useSheetState = (initial: boolean): [boolean, (state: boolean) => void] => {
     const [key] = useState(generateId(16))
     const [state, setState] = useState(initial)
     const setSheetState = (newState: boolean) => {
